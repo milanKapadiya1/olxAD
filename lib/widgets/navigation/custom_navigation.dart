91e027c6982c9f45bc49264adb728f18d781d14d
Milan <milankapadiya111@gmail.com>
2025-10-15 12:55:38 +0530

    update nav var

--- lib/widgets/navigation/custom_navigation.dart
+++ lib/widgets/navigation/custom_navigation.dart
@@ -11,157 +11,126 @@ class CustomBottomnav extends StatefulWidget {
 class _CustomBottomnavState extends State<CustomBottomnav> {
   int _currentNavIndex = 0;
 
-  void _ontiemTapped(int index) {
+  static const Color _selectedColor = Color.fromARGB(255, 88, 151, 223);
+  static const Color unselectedColor = Color(0xFFA2A2A2);
+
+  static const List<Color> _gradientColors = [
+    Color(0xFF34C759), 
+    Color(0xFF007AFF), 
+    Color(0xFFFFCC00),
+    Color(0xFF34C759), 
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
       _currentNavIndex = index;
     });
   }
 
-  static const Color _selectedColor = Color.fromARGB(255, 88, 151, 223);
-  static const Color unselectedColor = Color(0xFFA2A2A2);
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        backgroundColor: Colors.white,
-        selectedItemColor: _selectedColor,
-        unselectedItemColor: unselectedColor,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentNavIndex,
-        onTap: _ontiemTapped,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/Home.svg',
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(unselectedColor, BlendMode.srcIn),
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.bottomCenter,
+      children: [
+        BottomNavigationBar(
+          backgroundColor: Colors.white,
+          selectedItemColor: _selectedColor,
+          unselectedItemColor: unselectedColor,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentNavIndex,
+          onTap: _onItemTapped,
+          items: [
+            _buildItem('assets/icons/Home.svg', 0),
+            _buildItem('assets/icons/Heart.svg', 1),
+            // Placeholder for the center FAB
+            const BottomNavigationBarItem(
+              icon: SizedBox.shrink(),
+              label: '',
             ),
-            activeIcon: Column(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/icons/Home.svg',
-                  height: 24,
-                  width: 24,
-                  colorFilter:
-                      const ColorFilter.mode(_selectedColor, BlendMode.srcIn),
+            _buildItem('assets/icons/Bag.svg', 3),
+            _buildItem('assets/icons/Notification.svg', 4),
+          ],
+        ),
+        Positioned(
+          bottom: 24,
+          child: GestureDetector(
+            onTap: () {
+              print("Center button tapped");
+            },
+            child: Container(
+              height: 60, 
+              width: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: const SweepGradient(
+                  colors: _gradientColors,
+                  startAngle: 1.5708, // Start at the top (90 degrees or pi/2 radians)
+                  endAngle: 7.85398, // Sweep 2*pi
                 ),
-                SizedBox(
-                  height: 4,
-                ),
-                Container(
-                  height: 5,
-                  width: 10,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 74, 28, 238),
-                    borderRadius: BorderRadius.circular(18),
-                  ),
-                ),
-              ],
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/Heart.svg',
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(unselectedColor, BlendMode.srcIn),
-            ),
-            activeIcon: Column(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/icons/Heart.svg',
-                  height: 24,
-                  width: 24,
-                  colorFilter:
-                      const ColorFilter.mode(_selectedColor, BlendMode.srcIn),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Container(
-                  height: 5,
-                  width: 10,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 74, 28, 238),
-                    borderRadius: BorderRadius.circular(18),
-                  ),
-                ),
-              ],
-            ),
-            label: 'Heart',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/Bag.svg',
-              height: 24,
-              width: 24,
-              colorFilter: ColorFilter.mode(unselectedColor, BlendMode.srcIn),
-            ),
-            activeIcon: Column(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/icons/Bag.svg',
-                  height: 24,
-                  width: 24,
-                  colorFilter:
-                      const ColorFilter.mode(_selectedColor, BlendMode.srcIn),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Container(
-                  height: 5,
-                  width: 10,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 74, 28, 238),
-                    borderRadius: BorderRadius.circular(18),
-                  ),
-                ),
-              ],
-            ),
-            label: 'Bag',
-          ),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/Notification.svg',
-                height: 24,
-                width: 24,
-                colorFilter: ColorFilter.mode(unselectedColor, BlendMode.srcIn),
-              ),
-              activeIcon: Column(
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/Notification.svg',
-                    height: 24,
-                    width: 24,
-                    colorFilter:
-                        const ColorFilter.mode(_selectedColor, BlendMode.srcIn),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Container(
-                    height: 5,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 74, 28, 238),
-                      borderRadius: BorderRadius.circular(18),
-                    ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5), 
                   ),
                 ],
               ),
-              label: 'Notification')
-        ]);
+              child: Center(
+                child: Container(
+                  height:50, 
+                  width: 50,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.add,
+                    color: _selectedColor, 
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(String asset, int index) {
+    final isSelected = _currentNavIndex == index;
+
+    return BottomNavigationBarItem(
+      icon: SvgPicture.asset(
+        asset,
+        height: 24,
+        width: 24,
+        colorFilter: const ColorFilter.mode(unselectedColor, BlendMode.srcIn),
+      ),
+      activeIcon: Column(
+        children: [
+          SvgPicture.asset(
+            asset,
+            height: 24,
+            width: 24,
+            colorFilter:
+                const ColorFilter.mode(_selectedColor, BlendMode.srcIn),
+          ),
+          const SizedBox(height: 4),
+          Container(
+            height: 5,
+            width: 10,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 74, 28, 238),
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
+        ],
+      ),
+      label: '',
+    );
   }
 }
